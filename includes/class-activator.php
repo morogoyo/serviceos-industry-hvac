e1970133783f4f4c23792c27f804022060209aed
rtorres <rtorres@serviceos.com>
2026-07-08 21:31:21 -0400

    fix: create hvac_submissions tables on plugin activation, not first submission
    - class-activator.php: call Public_Checklist::create_tables() during activation
    - class-public.php: change create_tables() from private to public (exposed for activator)

--- includes/class-activator.php
+++ includes/class-activator.php
@@ -4,6 +4,7 @@ namespace ServiceOS_Industry_Plugin;
 class Activator {
     public static function activate() {
         update_option('serviceos_ip_seed_pending', true);
+        Public_Checklist::create_tables();
     }
 
     public static function deactivate() {
